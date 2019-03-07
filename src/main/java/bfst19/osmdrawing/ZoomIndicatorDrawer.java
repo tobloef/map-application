a15d98616f21c43076620e5522a1f567c393b608
Jakob Uttenthal Israelsen <jais@itu.dk>
2019-03-07 15:58:32 +0100

    Split zoomindicator into more methods, finalized look

--- src/main/java/bfst19/osmdrawing/ZoomIndicatorDrawer.java
+++ src/main/java/bfst19/osmdrawing/ZoomIndicatorDrawer.java
@@ -4,12 +4,18 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.scene.transform.Affine;
 
 
 public class ZoomIndicatorDrawer implements Drawer {
 	private MapCanvas canvas;
 	private GraphicsContext graphicsContext;
+	private int margin = 5;
+	private int boxWidth = 100;
+	private int boxHeight = 30;
+	private int bottomOffset = boxHeight - margin;
 
 	public ZoomIndicatorDrawer(MapCanvas canvas) {
 		this.canvas = canvas;
@@ -18,29 +24,42 @@ public class ZoomIndicatorDrawer implements Drawer {
 
 	@Override
 	public void draw() {
-		int margin = 5;
-		int boxWidth = 100;
-		int boxHeight = 30;
-		double distance = (boxHeight - (margin * 2)) * canvas.getZoomFactor() * 111;
-
-		Font font = new Font(10);
 		Affine affine = graphicsContext.getTransform();
 		double oldLineWidth = graphicsContext.getLineWidth();
 		graphicsContext.setTransform(new Affine());
-		graphicsContext.setStroke(Color.BLACK);
-		graphicsContext.setFill(Color.WHITE);
-		graphicsContext.fillRect(margin, canvas.getHeight() - (margin + boxHeight + boxHeight), boxWidth, boxHeight);
-		graphicsContext.setFill(Color.BLACK);
-		graphicsContext.setLineWidth(1);
-		graphicsContext.moveTo(margin + boxWidth - margin, margin + margin); //left line, top side
-		graphicsContext.lineTo(margin + margin, margin + margin); // left line, bottom side
-		graphicsContext.lineTo(margin + margin, margin + boxHeight - margin); //right line, bottom side
-		graphicsContext.lineTo(margin + boxWidth - margin, margin + boxHeight - margin); // right line, top side
-		graphicsContext.stroke();
-		graphicsContext.setLineWidth(0);
-		graphicsContext.setFont(font);
-		graphicsContext.strokeText(String.format("%.1f km", distance), margin + boxWidth / 2f - 2, margin + boxHeight / 2f);
+		drawIndicator();
 		graphicsContext.setLineWidth(oldLineWidth);
 		graphicsContext.setTransform(affine);
+	}
+
+	private void drawIndicator() {
+		drawBackground();
+		drawBlackBars();
+		drawText();
+	}
+
+	private void drawBackground() {
+		graphicsContext.setStroke(Color.BLACK);
+		graphicsContext.setFill(Color.WHITE);
+		graphicsContext.fillRect(margin, canvas.getHeight() - (margin + boxHeight + bottomOffset), boxWidth, boxHeight);
+	}
+
+	private void drawText() {
+		double distance = (boxWidth - (margin * 2)) * canvas.getZoomFactor() * 111;
+		Font font = new Font(10);
+		graphicsContext.setLineWidth(0);
+		graphicsContext.setFont(font);
+		graphicsContext.setTextAlign(TextAlignment.CENTER);
+		graphicsContext.strokeText(String.format("%.1f km", distance), margin + boxWidth / 2f, canvas.getHeight() - (bottomOffset + margin + boxHeight / 2f));
+	}
+
+	private void drawBlackBars() {
+		graphicsContext.setFill(Color.BLACK);
+		graphicsContext.setLineWidth(1);
+		graphicsContext.moveTo(margin + margin, canvas.getHeight() - (margin + boxHeight + bottomOffset) + margin); // left line, top side
+		graphicsContext.lineTo(margin + margin, canvas.getHeight() - (margin + boxHeight + bottomOffset) + boxHeight - margin); //left line, bottom side
+		graphicsContext.lineTo(margin + boxWidth - margin, canvas.getHeight() - (margin + boxHeight + bottomOffset) + boxHeight - margin); // right line, bottom side
+		graphicsContext.lineTo(margin + boxWidth - margin, canvas.getHeight() - (margin + boxHeight + bottomOffset) + margin); //right line, top side
+		graphicsContext.stroke();
 	}
 }
