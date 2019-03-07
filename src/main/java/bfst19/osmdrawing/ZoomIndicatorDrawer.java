f9286a880c9af10d14333b5be3b0fcbb79be6ce3
Jakob Uttenthal Israelsen <jais@itu.dk>
2019-03-07 13:40:08 +0100

    Now have the zoomIndicator, except it doesn't really indicate

--- src/main/java/bfst19/osmdrawing/ZoomIndicatorDrawer.java
+++ src/main/java/bfst19/osmdrawing/ZoomIndicatorDrawer.java
@@ -2,16 +2,20 @@ package bfst19.osmdrawing;
 
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.scene.transform.Affine;
+
 
 public class ZoomIndicatorDrawer implements Drawer {
 	private GraphicsContext graphicsContext;
-	private int margin = 10;
-	private int boxWidth = 30;
+	private int margin = 5;
+	private int boxWidth = 40;
 	private int boxHeight = 100;
+	Font font;
 
 	public ZoomIndicatorDrawer(GraphicsContext graphicsContext) {
 		this.graphicsContext = graphicsContext;
+		font = new Font(10);
 	}
 
 	@Override
@@ -22,12 +26,16 @@ public class ZoomIndicatorDrawer implements Drawer {
 		graphicsContext.setStroke(Color.BLACK);
 		graphicsContext.setFill(Color.WHITE);
 		graphicsContext.fillRect(margin, margin, boxWidth, boxHeight);
-		graphicsContext.setLineWidth(1);
 		graphicsContext.setFill(Color.BLACK);
+		graphicsContext.setLineWidth(1);
 		graphicsContext.moveTo(margin + boxWidth - margin, margin + margin); //top line, right side
 		graphicsContext.lineTo(margin + margin, margin + margin); // top line, left side
 		graphicsContext.lineTo(margin + margin, margin + boxHeight - margin); //bottom line, left side
 		graphicsContext.lineTo(margin + boxWidth - margin, margin + boxHeight - margin); // bottom line right side
+		graphicsContext.stroke();
+		graphicsContext.setLineWidth(0);
+		graphicsContext.setFont(font);
+		graphicsContext.strokeText("0", margin + boxWidth / 2f, margin + boxHeight / 2f);
 		graphicsContext.setLineWidth(oldLineWidth);
 		graphicsContext.setTransform(affine);
 	}
