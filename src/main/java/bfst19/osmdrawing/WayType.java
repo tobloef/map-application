cb4216ccd24da5d5575b9398beefe4ea6172370d
koab <koab@itu.dk>
2019-03-07 14:53:32 +0100

    Har tilføjet wayTypes men nu virker det ikkegit push!

--- src/main/java/bfst19/osmdrawing/WayType.java
+++ src/main/java/bfst19/osmdrawing/WayType.java
@@ -1,36 +1,65 @@
+
 package bfst19.osmdrawing;
 
 import javafx.scene.paint.Color;
-
+//The wayTypes are drawed in the order they are written in
 public enum WayType {
-	COASTLINE(Color.web("EEF0D5", 1.0),null ),
-	WATER(Color.LIGHTSKYBLUE, null),
-	ISLAND(Color.PINK, null),
-	BARRIER(null, Color.BROWN),
-	PITCH(Color.MEDIUMAQUAMARINE, null),
-	SHRUB(Color.LIGHTGREEN, null),
-	CANAL(Color.LIGHTSKYBLUE, null),
-	FERRY(null, Color.BLUE, 2),
-	TOUR(null, Color.BLACK, 2),
-	BRIDGE(Color.DARKKHAKI, Color.DARKKHAKI),
+	COASTLINE(Color.web("EEF0D5"),null ),
+
+	//Ø-ting
+	ISLAND(Color.web("F4F0EA"), null),
+	SCREE(Color.web("F1E5DC"),null),
+
+	//Natur
 	TREES(Color.DARKGREEN, null),
-	GRASS(Color.LAWNGREEN, null),
+	GRASS(Color.web("C4EDB2"), null),
+	SAND(Color.web("FFF1BA"), null),
+	FARMLAND(Color.web("EFF1D5"), null),
+	WOOD(Color.web("A1D29F"), null),
+	HEATH(Color.web("D6D99F"),null),
+	MEADOW(Color.web("C4EDB2"), null),
+	PARK(Color.LIGHTGREEN, null),
+	WETLAND(Color.LIGHTBLUE, null), //Skal have en tekstur
+	BROWNFIELD(Color.TAN, null),
+	SHRUB(Color.LIGHTGREEN, null),
+
+	//Bygningsting?
 	CONSTRUCTION(Color.DARKSEAGREEN, null),
 	INDUSTRIAL(Color.THISTLE, null),
 	CEMETERY(Color.OLIVEDRAB, null),
 	RESIDENTIAL(Color.LAVENDER, null),
-	BROWNFIELD(Color.TAN, null),
 	ALLOTMENTS(Color.PALEGREEN, null),
-	BASIN(Color.LIGHTSKYBLUE, null),
-	PARK(Color.LIGHTGREEN, null),
-	SWIMMINGPOOL(Color.LIGHTSKYBLUE, null),
 	PLAYGROUND(Color.MEDIUMAQUAMARINE, null),
-	INTERSTATE(Color.LIGHTGREY, null),
 	BUILDING(Color.DARKGRAY, null),
-	UNKNOWN(null, Color.BLACK),
-	RAILWAY(null, Color.LIGHTGRAY, 2),
+	BARRIER(null, Color.BROWN),
+	PITCH(Color.MEDIUMAQUAMARINE, null),
+	GROYNE(Color.web("A9A9A9"), null),
 	PIER(Color.YELLOW, Color.YELLOW),
-	IGNORE(null, null);
+
+	//Veje
+	RESIDENTIALROAD(null, Color.web("FFFFFF")), //Har ikke en tykkelse
+	SECONDARYHIGHWAY(null, Color.web("F8FBC0")), //Har ikke en tykkelse
+	RUNWAY(null, Color.web("BABACC")), //Har ikke en tykkelse
+	RAILWAY(null, Color.LIGHTGRAY, 2),
+	MOTORWAY(null, Color.web("FEB9C3")), //Har ikke en tykkelse
+	SECONDARYROAD(null, Color.web("FFAD95")), //Har ikke en tykkelse
+	BRIDGE(Color.DARKKHAKI, Color.DARKKHAKI),
+
+	//Abstrakte ting såsom ruter
+	MARKING(Color.BLACK, null),
+	TRACK(null, Color.web("B68235"), 2),
+	FERRY(null, Color.BLUE, 2),
+	TOUR(null, Color.BLACK, 2),
+
+	//Vand
+	WATER(Color.LIGHTSKYBLUE, null),
+	SWIMMINGPOOL(Color.LIGHTSKYBLUE, null),
+	CANAL(Color.LIGHTSKYBLUE, null),
+	BASIN(Color.LIGHTSKYBLUE, null),
+
+	IGNORE(null, null),
+	UNKNOWN(null, Color.BLACK);
+
 
 	private double lineDash;
 	private Color fillColor;
