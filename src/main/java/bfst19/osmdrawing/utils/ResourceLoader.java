0416125dfeac29dbf0862e6c0c72a9142c36fd16
Tobias Løfgren <tobl@itu.dk>
2019-03-09 23:19:03 +0100

    Add comment about class

--- src/main/java/bfst19/osmdrawing/utils/ResourceLoader.java
+++ src/main/java/bfst19/osmdrawing/utils/ResourceLoader.java
@@ -5,6 +5,9 @@ import bfst19.osmdrawing.Main;
 import java.io.InputStream;
 import java.net.URL;
 
+// We use this helper-class instead of the normal "getClass().getResource()" approach, since the
+// path is different depending on whether we're running the program from a .jar file or from Gradle.
+// For .jar files we need the leading slash, but that won't work if we use Gradle.
 public class ResourceLoader {
     public static URL getResource(String name) {
         URL url = Main.class.getResource(name);
