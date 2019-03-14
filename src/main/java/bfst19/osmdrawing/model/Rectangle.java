f741858b463d0afc36d9c18729023e87fe761a3c
Hjalte Mac Dalland - Laptop <hjaltesorgenfrei@gmail.com>
2019-03-14 14:49:05 +0100

    Updated rectangle

--- src/main/java/bfst19/osmdrawing/model/Rectangle.java
+++ src/main/java/bfst19/osmdrawing/model/Rectangle.java
@@ -1,6 +1,7 @@
 package bfst19.osmdrawing.model;
 
 import java.io.Serializable;
+import java.util.Collection;
 
 public class Rectangle implements Serializable {
 	public float xmin, ymin, xmax, ymax;
@@ -19,5 +20,33 @@ public class Rectangle implements Serializable {
 	@Override
 	public String toString() {
 		return "" + xmin + " " + ymin + " " + xmax + " " + ymax;
+	}
+
+	public boolean intersect(Rectangle rectB){
+		return intersect(rectB.xmin, rectB.ymin, rectB.xmax, rectB.ymax);
+	}
+
+	public boolean intersect(float xmin, float ymin, float xmax,float ymax){
+		if (this.xmin < xmax && this.xmax > xmin &&
+				this.ymax > ymin && this.ymin < ymax ){
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	public boolean contains(Rectangle rectB){
+		return contains(rectB.xmin, rectB.ymin, rectB.xmax, rectB.ymax);
+	}
+
+	public boolean contains(float xmin, float ymin, float xmax,float ymax){
+		if (this.xmin < xmin && this.xmax > xmax &&
+				this.ymin < ymin && this.ymax > ymax ){
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 }
