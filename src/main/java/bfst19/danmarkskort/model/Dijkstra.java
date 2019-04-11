284791cb439535ac84e714383549e3efcf6a7b68
koab <koab@itu.dk>
2019-04-11 12:56:23 +0200

    Dijkstra virker i teorien

--- src/main/java/bfst19/danmarkskort/model/Dijkstra.java
+++ src/main/java/bfst19/danmarkskort/model/Dijkstra.java
@@ -10,54 +10,46 @@ public class Dijkstra {
 	public List<PolyRoad> getShortestPath(PolyRoad origin, PolyRoad destination){
 		double[] distTo = new double[PolyRoad.allPolyRoads.length];
 		HashMap<PolyRoad, PolyRoad> previousRoads = new HashMap<>();
-		PriorityQueue<RoadWithDistance> remainingPolyRoads = new PriorityQueue<>();
+		IndexMinPQ<Double> remainingPolyRoads = new IndexMinPQ<>(PolyRoad.allPolyRoads.length);
 
 		for(double distance : distTo){
 			distance = Double.POSITIVE_INFINITY;
 		}
 
 		distTo[origin.getIndex()] = 0;
-		remainingPolyRoads.add(new RoadWithDistance(origin, 0.0));
+		remainingPolyRoads.insert(origin.getIndex(), 0.0);
 		while(!remainingPolyRoads.isEmpty()){
-			RoadWithDistance current = remainingPolyRoads.poll();
+			PolyRoad current = PolyRoad.allPolyRoads[remainingPolyRoads.delMin()];
 			List<PolyRoad> connections = new ArrayList<>();
 
-			if(current.polyRoad == origin){
-				connections.addAll(current.polyRoad.getFirstConnections());
-				connections.addAll(current.polyRoad.getLastConnections());
+			if(current == origin){
+				connections.addAll(current.getFirstConnections());
+				connections.addAll(current.getLastConnections());
 			}
 			else{
-				connections.addAll(current.polyRoad.getOtherConnections(current.polyRoad));
+				connections.addAll(current.getOtherConnections(current));
 			}
 			for(PolyRoad thisConnection : connections){
-				if(distTo[thisConnection.getIndex()] > distTo[current.polyRoad.getIndex()] + current.distance){
-					distTo[thisConnection.getIndex()] = distTo[current.polyRoad.getIndex()] + current.distance;
-					previousRoads.put(thisConnection, current.polyRoad);
-					if(remainingPolyRoads.remove())
+				int w = thisConnection.getIndex();
+				if(distTo[w] > distTo[current.getIndex()] + current.getLength()){
+					distTo[w] = distTo[current.getIndex()] + current.getLength();
+					previousRoads.put(thisConnection, current);
+					if(remainingPolyRoads.contains(w)){
+						remainingPolyRoads.changeKey(w, distTo[w]);
+					}
+					else{
+						remainingPolyRoads.insert(w, distTo[w]);
+					}
+				}
+				if(PolyRoad.allPolyRoads[w] == destination){
+					List<PolyRoad> path = new ArrayList<>();
+					while(path.get(path.size()-1) != origin){
+						path.add(previousRoads.get(path.get(path.size()-1)));
+					}
+					return path;
 				}
 			}
 		}
-
-	}
-
-	private class RoadWithDistance implements Comparable<RoadWithDistance>{
-		public PolyRoad polyRoad;
-		public double distance;
-
-		public RoadWithDistance(PolyRoad polyRoad, double distance) {
-			this.polyRoad = polyRoad;
-			this.distance = distance;
-		}
-
-		@Override
-		public int compareTo(RoadWithDistance o) {
-			if(distance < o.distance){
-				return -1;
-			}
-			if(distance == o.distance){
-				return 0;
-			}
-			return 1;
-		}
+		throw new IllegalArgumentException("There is no connection between the two roads");
 	}
 }
