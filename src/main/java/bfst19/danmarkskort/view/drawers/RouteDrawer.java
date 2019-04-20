7fcc2cf0f21ab1d0bf47ba8432457c2c1f45cb5e
Jakob Uttenthal Israelsen <jais@itu.dk>
2019-04-20 07:46:58 +0200

    Map now shows all roads looked at by dijkstra

--- src/main/java/bfst19/danmarkskort/view/drawers/RouteDrawer.java
+++ src/main/java/bfst19/danmarkskort/view/drawers/RouteDrawer.java
@@ -3,6 +3,7 @@ package bfst19.danmarkskort.view.drawers;
 import bfst19.danmarkskort.model.*;
 import bfst19.danmarkskort.view.controls.MapCanvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +13,7 @@ import static bfst19.danmarkskort.utils.ThemeLoader.loadTheme;
 public class RouteDrawer implements Drawer{
 	private MapCanvas canvas;
 	private Model model;
+	private boolean debugging = true;
 
 	public RouteDrawer(MapCanvas canvas, Model model) {
 		this.canvas = canvas;
@@ -23,6 +25,14 @@ public class RouteDrawer implements Drawer{
 		Theme theme = loadTheme("config/themes/default.yaml", null);
 		GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
 		graphicsContext.setLineWidth(theme.getDrawingInfo(WayType.RESIDENTIAL_ROAD).getLineWidth() * 4);
+		if (debugging) {
+			graphicsContext.save();
+			graphicsContext.setStroke(Color.RED);
+			for (PolyRoad road : Dijkstra.lastUsedRoads) {
+				road.stroke(graphicsContext, canvas.getDegreesLatitudePerPixel());
+			}
+			graphicsContext.restore();
+		}
 		for (Drawable drawable : model.getShortestPath()){
 			drawable.stroke(graphicsContext, canvas.getDegreesLatitudePerPixel());
 		}
