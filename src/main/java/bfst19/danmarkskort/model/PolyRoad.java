bfdb1595ebb1463639f192324bead746eb2259fe
Jakob Uttenthal Israelsen <jais@itu.dk>
2019-04-25 00:09:30 +0200

    Trying to test if degrees work properly

--- src/main/java/bfst19/danmarkskort/model/PolyRoad.java
+++ src/main/java/bfst19/danmarkskort/model/PolyRoad.java
@@ -120,6 +120,21 @@ public class PolyRoad extends Polyline implements Serializable {
 		return result;
 	}
 
+	public double getDurationInMinutes() {
+		return getLength() * 110 / getSpeedLimit() * 60;
+	}
+
+	public double getDegree(boolean last) {
+		double deltaX = coords[2] - coords[0];
+		double deltaY = coords[3] - coords[1];
+		if (last) {
+			int end = coords.length;
+			deltaX = coords[end-2] - coords[end-4];
+			deltaY = coords[end-1] - coords[end-3];
+		}
+		return Math.atan(deltaY / deltaX);
+	}
+
 	public double getSpeedLimit() {
 		return speedLimit;
 	}
--- src/main/java/bfst19/danmarkskort/model/Route.java
+++ src/main/java/bfst19/danmarkskort/model/Route.java
@@ -11,9 +11,30 @@ public class Route extends ArrayList<PolyRoad> {
 		return stream().mapToDouble(PolyRoad::getLength).sum();
 	}
 
+	public double sumTime() {
+		return stream().mapToDouble(PolyRoad::getDurationInMinutes).sum();
+	}
+
 	public List<String> getTextDescription() {
 		//fixme x.tostring should be replaced with the name of the road
-		return stream().map(x -> ("Drive on " + x.toString() + " for " + (x.getLength()*110 / x.getSpeedLimit())*60 + "minutes" )).collect(Collectors.toList());
+		List<String> result = new ArrayList<>();
+		PolyRoad last = null;
+		for (PolyRoad road : this) {
+			String description = "";
+			int direction = getDirection(last, road);
+			last = road;
+			result.add("Drive on " + road.toString() + " for " + road.getDurationInMinutes() + "minutes");
+		}
+		return result;
+		//return stream().map(x -> ("Drive on " + x.toString() + " for " + (x.getLength()*110 / x.getSpeedLimit())*60 + "minutes" )).collect(Collectors.toList());
+	}
+
+	private int getDirection(PolyRoad last, PolyRoad current) {
+		double lastDegree = last.getDegree(true);
+		double firstDegree = current.getDegree(false);
+		System.out.println(lastDegree);
+		System.out.println(firstDegree);
+		return 0;
 	}
 
 	public static void printAll(List<String> list) {
