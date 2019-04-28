d654354f821cc5ab156109cc9561596d98794e76
Tobias Løfgren <tobloef@gmail.com>
2019-04-28 22:02:24 +0200

    Add getWithFallback

--- src/main/java/bfst19/danmarkskort/utils/Misc.java
+++ src/main/java/bfst19/danmarkskort/utils/Misc.java
@@ -1,10 +1,21 @@
 package bfst19.danmarkskort.utils;
 
+import java.util.Map;
+
 public class Misc {
     public static <T> T pickNotNull(T first, T second) {
         if (first != null) {
             return first;
         }
         return second;
+    }
+
+    public static String getWithFallback(Map<String, String> map, String[] keys) {
+        for (String key : keys) {
+            if (map.containsKey(key)) {
+                return map.get(key);
+            }
+        }
+        return null;
     }
 }
